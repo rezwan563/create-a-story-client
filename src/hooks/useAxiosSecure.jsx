19a0b8c14754db4c23fbe8a2c3b775cdafeaa42c
Rezwan <t.rewan17@gmail.com>
2023-06-11 11:49:59 +0600

    Solved issue with useAxiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,39 +1,83 @@
-import axios from "axios";
-import { useContext, useEffect } from "react";
-import { AuthContext } from "../pages/provider/AuthProvider";
-import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import useAuth from "./useAuth";
+// import { useEffect } from "react";
+
+
+
+// const useAxiosSecure = () => {
+//   const { logOut } = useAuth();
+//   const navigate = useNavigate();
+ 
+//   const axiosSecure = axios.create({
+//     baseURL: "http://localhost:5000",
+//   });
+
+//   useEffect(() => {
+//     axiosSecure.interceptors.request.use((config) => {
+//       const token = localStorage.getItem("access-token");
+//       if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//       }
+//       return config;
+//     });
+
+//     axiosSecure.interceptors.response.use((response) => {
+//       response,
+//         async (error) => {
+//           if (
+//             error.response &&
+//             (error.response.status === 401 || error.response.status === 403)
+//           ) {
+//             await logOut();
+//             navigate("/login");
+//           }
+//           return Promise.reject(error);
+//         };
+//     }
+//     );
+//   }, [logOut, navigate, axiosSecure]);
+//   return [axiosSecure];
+// };
+
+// export default useAxiosSecure;
+
+// N.B. There was an error in the above code. I couldn't solve it. I copied the below code from the modules git repo. It is working now
+
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: 'http://localhost:5000', 
 });
 
 const useAxiosSecure = () => {
-  const { logOut } = useContext(AuthContext);
-  const navigate = useNavigate();
+  const { logOut } = useAuth(); 
+  const navigate = useNavigate(); 
 
   useEffect(() => {
     axiosSecure.interceptors.request.use((config) => {
-      const token = localStorage.getItem("access-token");
+      const token = localStorage.getItem('access-token');
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
       }
       return config;
     });
 
-    axiosSecure.interceptors.response.use((response) => {
-      response,
-        async (error) => {
-          if (
-            error.response &&
-            (error.response.status === 401 || error.response.status === 403)
-          ) {
-            await logOut();
-            navigate("/login");
-          }
-          return Promise.reject(error);
-        };
-    });
+    axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+          await logOut();
+          navigate('/login');
+        }
+        return Promise.reject(error);
+      }
+    );
   }, [logOut, navigate]);
+
   return [axiosSecure];
 };
 
--- src/hooks/useSelectClass.jsx
+++ src/hooks/useSelectClass.jsx
@@ -1,18 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
-import { AuthContext } from "../pages/provider/AuthProvider";
 import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
 
 const useSelectClass = ()=>{
-    const { user} = useContext(AuthContext)
-    const token = localStorage.getItem('access-token')
+    const { user, loading} = useAuth()
+    // const token = localStorage.getItem('access-token')
     const [axiosSecure] = useAxiosSecure()
     const {data: selectedClasses = [], isLoading, refetch} = useQuery({
         queryKey:['select_classes', user?.email],
+        enabled: !loading,
+        // queryFn: async()=>{
+        //     const res = await fetch(`http://localhost:5000/select_classes?email=${user?.email}`, {
+        //         headers:{
+        //             authorization: `bearer ${token}`
+        //         }
+        //     })
+        //     return res.json()
+        // }
         queryFn: async()=>{
             const res = await axiosSecure(`/select_classes?email=${user?.email}`)
-            console.log('res from axios', res)
-            return res.data
+            console.log("from res ", res)
+            return res.data;
         }
     })
     return [selectedClasses, refetch, isLoading]
